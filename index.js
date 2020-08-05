946ae7a98f328c220cb55916a2ecb979f7f4456c
Taley'a Mirza <34168281+taleymirza@users.noreply.github.com>
2020-08-05 15:00:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const employees = [
+  {
+    name: "Ben",
+    title: "Software developer",
+    institute: "Harvard University",
+    email: "",
+    socialMedia: [
+      {
+        dribble: {
+          url: "",
+          cssClasses: "",
+        },
+        twitter: {
+          url: "",
+          cssClasses: "",
+        },
+        linkedIn: {
+          url: "",
+          cssClasses: "",
+        },
+        facebook: {
+          url: "",
+          cssClasses: "",
+        },
+      },
+    ],
+  },
+];
